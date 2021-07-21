351805905d4d0b05e9da266c195224209e2aad85
chiaralina <chiaralinacasadei@gmail.com>
2021-07-21 10:44:47 -0300

    Generic MVC Version, no details added

---
+++ BlogWarmUpSol/BlogWarmUp/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogWarmUp.Models
+{
+    public class Post
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        //imagen
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
